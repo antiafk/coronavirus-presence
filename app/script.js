7f1047aa5f939a2d15bb32c4aca9d797fa6a1f88
antiafk <42821200+antiafk@users.noreply.github.com>
2021-06-03 15:46:55 -0300

    code improvements

--- app/script.js
+++ app/script.js
@@ -2,7 +2,7 @@ const remote = require("electron").remote,
 windows = remote.require("./src/windows"),
 presence = remote.require("./src/presence");
 
-let enabled = true;
+let enabled = false;
 
 // config
 let showGitHub = document.getElementById("showgithub"),
@@ -10,6 +10,8 @@ showUpdateTime = document.getElementById("showupdatetime"),
 updateTime = document.getElementById("updatetime")
 toggleText = document.getElementById("toggle-text")
 toggleButton = document.getElementById("toggle");
+
+let reconnectTimer;
 
 function changesWarning() {
     if (enabled) {
@@ -19,24 +21,27 @@ function changesWarning() {
 }
 
 function changePresence() {
-    enabled = toggleButton.checked;
-
-    if (enabled) {
+    if (toggleButton.checked) {
         toggleText.innerHTML = "<i>Trying to connect...</i>";
         toggleText.style.color = "#fff";
         
+        if (enabled) presence.disable();
+        
         presence.enable({ showGitHub: showGitHub.checked, showUpdateTime: showUpdateTime.checked, updateTime: updateTime.value * 60000 })
         .then(() => {
+			enabled = true;
             toggleText.innerHTML = "Connected.";
             toggleText.style.color = "#52ff57";
         })
-        .catch(() => {
-            setTimeout(changePresence, 5000);
+        .catch(e => {
+            console.error(e);
+            reconnectTimer = setTimeout(changePresence, 5000);
         });
     } else {
         toggleText.innerHTML = "Disconnected.";
         toggleText.style.color = "#ff5252";
-        presence.disable();
+        if (enabled) presence.disable();
+        if (reconnectTimer) clearTimeout(reconnectTimer);
     }
 }
 
